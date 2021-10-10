aa96416d72b993e696739eeba11349b10a727ffd
pawk <alezowszem@gmail.com>
2021-10-10 15:26:40 +0200

    more

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Use it with simple `npm start`. It will rendomly pick a single image for all the defined attributes and merge them into one. Modify `config.js` to change the configuration.
 
 By default, output files are written into `./art` directory, beware it will be cleare with every program execution.
+
+To see it in action, just run it and inspect `./art` directory.
--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const ART_DIR = './art'
 // how many art you want to generate
 const ART_COUNT = 10
 // classes will be translated to directory paths of $ATTRIBUTE_DIR/$ATTRIBUTE_CLASS, i.e.: ./images/attr1
-const ATTRIBUTE_CLASSES = ['attr1', 'attr2', 'attr3', 'attr4']
+const ATTRIBUTE_CLASSES = ['body', 'eyes', 'mouth', 'extra']
 // directory where you hold ATTRIBUTE_CLASSES directories
 const ATTRIBUTE_DIR = './images'
 // number of milliseconds between art generations
