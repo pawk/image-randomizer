cef3c40c017e5801c66f20beb3ea5d82320a6ff4
pawk <alezowszem@gmail.com>
2021-10-10 15:08:33 +0200

    pick only png files

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ const ART_DIR = './art'
 // how many art you want to generate
 const ART_COUNT = 10
 // classes will be translated to directory paths of $ATTRIBUTE_DIR/$ATTRIBUTE_CLASS, i.e.: ./images/attr1
-const ATTRIBUTE_CLASSES = ['attr1', 'attr4', 'attr3']
+const ATTRIBUTE_CLASSES = ['attr1', 'attr2', 'attr3', 'attr4']
 // directory where you hold ATTRIBUTE_CLASSES directories
 const ATTRIBUTE_DIR = './images'
 // number of milliseconds between art generations
-const DELAY = 1000
+const DELAY = 0
+
+const isPng = filepath => filepath.endsWith('.png')
 
 const getRandomImage = attribute => {
     const attributeDir = `${ATTRIBUTE_DIR}/${attribute}`
     return readdir(attributeDir)
+        .then(items => items.filter(isPng))
         .then(items => `${attributeDir}/${items[Math.floor(Math.random()*items.length)]}`)
 }
 
